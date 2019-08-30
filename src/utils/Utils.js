a1a749a3753bcd3b6b551a23890a05c59bd212e0
Werdffelynir <werdffelynir@gmail.com>
2019-08-30 18:33:51 +0300

    added responsive to section What We Do

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,6 +1,10 @@
 
+const MOBILE_BREAKPOINT = 550;
+
 export const isBrowser = () => typeof window !== undefined;
 
 export const getWidth = () => isBrowser() ? window.innerWidth : 0;
+
+export const isMobile = () => getWidth() < MOBILE_BREAKPOINT;
 
 
